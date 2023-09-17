ee177a5b40df5a5f95546f9a312d52a2acd398f8
xkcore123 <70746008+xkcore123@users.noreply.github.com>
2023-09-17 17:47:11 +0800

    Update diy-part2.sh

--- scripts/common/diy-part2.sh
+++ scripts/common/diy-part2.sh
@@ -11,5 +11,5 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.10.254/g' package/base-files/files/bin/config_generate
 
